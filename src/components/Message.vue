e558d92aadefdd57924f7e730245018036eda0bf
dragos <tuticadragos90@gmail.com>
2019-12-07 15:50:59 +0200

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about message">
+    <div>
+       Name: <input type="text" v-model="name" />
+    </div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'message',
+  data () {
+    return {
+      name: ''
+    }
+  },
+  watch: {
+    'name': function () {
+      this.callMe()
+    }
+  },
+  methods: {
+    // to update name value in parent component
+    callMe () {
+      this.$emit('name', this.name)
+    }
+  }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page. You can check parent-child binding</h1>
+    <Message @name="getName" />
+    <br/><br/>
+    <u>Message from Child</u>: {{name}}
+  </div>
+</template>
+<script>
+import Message from '@/components/Message.vue'
+export default {
+  name: 'about',
+  components: {
+    Message
+  },
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    // Recieve value from child
+    getName (val) {
+      //    set name value
+      this.name = val
+    }
+  }
+}
+</script>
